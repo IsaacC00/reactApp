e23f8a98876ac4a9b3f97e1d13d7dcd4d77082f3
IsaacC00 <isaacpasquel1974@gmail.com>
2024-02-20 00:26:31 -0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,102 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
+
+//tipos dar valores con lo que quiero trabajar
+//permite trbajar con data primitiva con data nueva creada por el dev
+//tipo fr datos esperados
+type gender = "male" | "female" | "no-binario"
+
+interface PersonInterface{
+  name:string;
+  lastNmae:string;
+  email : string | null;
+  age: number;
+  gender: gender;
+  isActive: boolean;
+
+}
+  //funcion para aumenta la edad de una persona                si no recive nada spor defecto le pasa 9
+  const incrementeAgePerson = (person: PersonInterface, ageNumbre: number = 9) => {
+    const newAge = person.age + ageNumbre; 
+    //console.log(person.age);|
+    
+    const newPerson: PersonInterface = {
+      age:newAge,
+      name:person.name,
+      lastNmae:person.lastNmae,
+      email:person.email,
+      gender:person.gender,
+      isActive:person.isActive
+      
+    }
+    return newPerson;
+  }
+
+  //cracion del objeto 
+  let persona:PersonInterface = {
+    name: "Isaac",
+    lastNmae: "Calderon",
+    email:null,
+    age: 23,
+    gender:"male",
+    isActive: true,
+
+  }
+
+  console.log(incrementeAgePerson(persona,1));
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text
+      //importar estilos
+      style={styles.text}
+      >Hola Mundo desde react native</Text>
+      <Button
+        onPress={ () =>{
+          incrementeAgePerson(persona);
+          console.log("Ejecutando la funcion de persona ");
+          console.log({persona});
+          
+        }}
+        title="Presioname"
+        color="#841584"
+        
+      />
+    <Image
+    style={styles.tinyLogo}
+    source={
+      {uri:"https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png"}} 
+    />
+
+      <Text>{persona.name}</Text>
+      <Text>{persona.lastNmae}</Text>
+      <Text>{persona.age}</Text>
+      <Text>{persona.gender ==="male" ? "masculino" : 
+        persona.gender === "female" ? "femenino":
+          "no binario"
+      }</Text>
+      <Text>{persona.email}</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
 }
-
+//h+oja de estilos de javascript
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+      flex: 1,
+      width:"100%",
+      height:"100%",
+      backgroundColor: '#A59F99',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text:{
+      fontSize: 24,
+      color: "#FF8000"
+    },
+    tinyLogo:{
+      width:"20%",
+    }
+  });
