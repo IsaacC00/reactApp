663b502b0cd61e48da7644bbb61456c61aac7376
Isaac <isaacpasquel1974@gmail.com>
2024-02-21 16:55:28 -0500

    utilizacion de hooks

--- App.tsx
+++ App.tsx
@@ -1,166 +1,22 @@
 //comporbar git
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-
-  //hook per o con useState
-  const useConter = ()=>{
-    const [counter,setCounter] = useState(0)
-  
-    const increase = () => setCounter(counter +1);
-    const decrease = () => setCounter(counter -1);
-    const reset = () => setCounter(0);
-    
-    return{
-      counter,
-      increase,
-      decrease,
-      reset
-    }
-  }
-//hook per o con useState
+import React from 'react';
+import { FadeInImage } from './src/components/FadeInImage';
+import { View } from 'react-native';
   
 export default function App() {
-//tipos dar valores con lo que quiero trabajar
-//permite trbajar con data primitiva con data nueva creada por el dev
-//tipo fr datos esperados
-type gender = "male" | "female" | "no-binario"
-
-interface PersonInterface{
-  name:string;
-  lastName:string;
-  email : string | null;
-  age: number;
-  gender: gender;
-  isActive: boolean;
-
-}
-  //funcion para aumenta la edad de una persona                si no recive nada spor defecto le pasa 9
-  const incrementeAgePerson = (person: PersonInterface, ageNumbre: number = 9) => {
-    const newAge = person.age + ageNumbre; 
-    //console.log(person.age);|
-    
-    const newPerson: PersonInterface = {
-      age:newAge,
-      name:person.name,
-      lastName:person.lastName,
-      email:person.email,
-      gender:person.gender,
-      isActive:person.isActive
-      
-    }
-    return newPerson;
-  }
-
-  //cracion del objeto 
-  let persona:PersonInterface = {
-    name: "Isaac",
-    lastName: "Calderon",
-    email:null,
-    age: 23,
-    gender:"male",
-    isActive: true,
-  }
-
-  //descomposicion de un objeto 
-  const {name,lastName} = persona
-  console.log(name,lastName);
-  console.log(incrementeAgePerson(persona,1));
-  
-  const {counter,increase,decrease,reset} = useConter()
-
-  //para impplementar dos contadores, serrai de la siguiente forma
-  const conterOne = useConter();
-  const conterTwo = useConter(); 
 
   return (
-    <View style={styles.container}>
-      <Text>primer counter</Text>
+    <View >
+      <FadeInImage uri={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png'} 
+      style={
+        {
+          width: 100,
+          height: 100,
+          
+          alignSelf:'center' 
+        }
+      }/>
 
-      <Text>{conterOne.counter}</Text>
-      <Button onPress={conterOne.increase}
-        title='+'
-        
-      />
-      <Button onPress={conterOne.decrease}
-        title='-'
-      />
-      <Button onPress={conterOne.reset}
-        title='Reset'
-      />
-      
-      <Text>segundo counter</Text>
-      
-      <Text>{conterTwo.counter}</Text>
-      <Button onPress={conterTwo.increase}
-        title='+'
-        
-      />
-      <Button onPress={conterTwo.decrease}
-        title='-'
-      />
-      <Button onPress={conterTwo.reset}
-        title='Reset'
-      />
-
-      <Text
-      //importar estilos
-      style={styles.text}
-      >Hola Mundo desde react</Text>
-
-      <Button 
-        onPress={ () =>{
-          console.log("Ejecutando la funcion de persona ");
-          console.log(incrementeAgePerson(persona,1));
-        }}
-        title="Presioname"
-        color="#841584"
-        
-      />
-    
-    <TouchableOpacity onPress={
-      ()=> console.log("Click en Imagen")
-    }>
-
-      <Image
-        style={styles.tinyLogo}
-        source={{
-          uri: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'
-        }}
-      />
-    </TouchableOpacity>
-      
-      <Text>{persona.name}</Text>
-      <Text>{persona.lastName}</Text>
-      <Text>{persona.age}</Text>
-      <Text>{persona.gender ==="male" ? "masculino" : 
-        persona.gender === "female" ? "femenino":
-          "no binario"
-      }</Text>
-      <Text>{persona.email}</Text>
-
-      <StatusBar style="auto" />
     </View>
   );
 }
-//hoja de estilos de javascript
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      width:"100%",
-      height:"100%",
-      backgroundColor: '#A59F99',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text:{
-      fontSize: 24,
-      color: "#841584",
-      marginBottom: 10
-    },
-    tinyLogo:{
-      width:100,
-      height:100,
-      marginVertical: 20
-    },
-  });
---
+++ src/components/FadeInImage.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {useState } from "react";
+import {ActivityIndicator, Animated, ImageErrorEventData, ImageStyle, NativeSyntheticEvent, StyleProp, View } from "react-native"
+import { useAnimation } from "../hooks/useAnimation";
+
+interface Props{
+    uri:string;
+    //trbajamos con todoas las propeidad de estilos de imagen 
+    style:StyleProp<ImageStyle>
+}
+
+export const FadeInImage = ({uri,style}:Props)=>{
+    
+    console.log({uri});
+    const [isLoading,  setIsLoading] = useState(true);
+    const {opacity,fadeIn} = useAnimation();
+
+    const finishLoading = () =>{
+        setIsLoading(false);
+        fadeIn();
+    }
+    
+    //parar al moemento de no haber inernet 
+    //no renderizar al momento que no tenemos internet
+    //contemplamos el error
+ 
+    const onError = (err: NativeSyntheticEvent<ImageErrorEventData>) =>{
+        console.warn({err});
+        setIsLoading(false);
+    };
+
+    return (
+        <View
+            style={{justifyContent:"center", alignItems:"center"}}
+        >
+            {
+                isLoading 
+                && <ActivityIndicator
+                    style={{position:"absolute"}}
+                    color="grey"
+                    size={30}
+                /> 
+            }
+            <Animated.Image 
+            source={{uri}} 
+            onError={onError} 
+            onLoad={finishLoading} 
+            style={{...style as any, 
+            opacity
+            }}/>
+
+        </View>
+
+    );
+
+        
+
+    
+}
